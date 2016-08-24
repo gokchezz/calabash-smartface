be1904c38a3f73efed21337d9bc00131d89d2df7
Gokce Akcan <gokce.akcan@smartface.io>
2016-08-24 11:57:52 +0300

    Deleting unnecessary folder

--- lib/ios_build_scripts/BuildIpa.sh
+++
@@ -1,2 +0,0 @@
-xcodebuild archive -project Smartface.xcodeproj -scheme SmartfaceDemo -archivePath Smartface.xcarchive
-xcodebuild -exportArchive -archivePath Smartface.xcarchive -exportPath Smartface -exportFormat ipa -exportProvisioningProfile "smf-inhouse"
